b2973435fec1eda98205d95985b00efa5d61ab84
Said Katana <juniorkatana06@gmail.com>
2024-06-29 19:29:56 +0300

    First commit

---
+++ music-list.js
@@ -0,0 +1,182 @@
+let allMusic = [
+  {
+    name: "Eastside",
+    artist: "Benny Blanco X Halsey X Khalid",
+    img: "a2",
+    src: "east"
+  },
+  {
+    name: "The Reason Why",
+    artist: "JP Cooper X Stefflon Don X Ranx & Banx",
+    img: "a3",
+    src: "jp"
+  },
+  {
+    name: "Do you mind",
+    artist: "Dj Khaled X Nicki Minaj X Jeremih X August Alsina X Future X Rick Ross",
+    img: "a4",
+    src: "dj"
+  },
+  {
+    name: "Never Know",
+    artist: "6LACK",
+    img: "a5",
+    src: "lack"
+  },
+  {
+    name: "Versace on the floor",
+    artist: "Bruno Mars",
+    img: "a6",
+    src: "floor"
+  },
+  {
+    name: "Disconnect",
+    artist: "Harmonize X Mario",
+    img: "a7",
+    src: "disconnect"
+  },
+  {
+    name: "Mama Amina",
+    artist: "Mario X Sho Madjozi X Bontle Smith",
+    img: "a8",
+    src: "amina"
+  },
+  {
+    name: "Mnike",
+    artist: "Tyler ICU & Tumelo.za X Dj Maphorisa X Nandipha808 X Ceeka RSA & Tyron Dee ",
+    img: "a9",
+    src: "mnike"
+  },
+  {
+    name: "Ojapiano",
+    artist: "Kcee",
+    img: "a10",
+    src: "kcee"
+  },
+  {
+    name: "Shu",
+    artist: "Diamond Platnumz X Chley",
+    img: "a11",
+    src: "shu"
+  },
+  {
+    name: "Najuta",
+    artist: "Sanaipei Tande",
+    img: "a12",
+    src: "najuta"
+  },
+  {
+    name: "Hawataweza",
+    artist: "Wanavokali",
+    img: "a13",
+    src: "hawa"
+  },
+  {
+    name: "Tobasco 2",
+    artist: "Shekina Karen X King Kaka X Stella Mwangi X Vallerie Muthoni",
+    img: "a14",
+    src: "kaka"
+  },
+  {
+    name: "Wavy",
+    artist: "Khaligraph Jones X Sarkodie",
+    img: "a15",
+    src: "wavy"
+  },
+  {
+    name: "M E N U",
+    artist: "Mpthr33 X Sean NMG",
+    img: "a16",
+    src: "menu"
+  },
+  {
+    name: "I'm Alive",
+    artist: "Celine Dion",
+    img: "a17",
+    src: "alive"
+  },
+  {
+    name: "You sang to me",
+    artist: "Marc Anthony",
+    img: "a18",
+    src: "sang"
+  },
+  {
+    name: "Take on me",
+    artist: "a-ha",
+    img: "a19",
+    src: "aha"
+  },
+  {
+    name: "Nothing's gonna stop us",
+    artist: "Starship",
+    img: "a20",
+    src: "nothing"
+  },
+  {
+    name: "What a feeling",
+    artist: "Irene Cara",
+    img: "a21",
+    src: "irene"
+  },
+  {
+    name: "Whatever it takes",
+    artist: "Imagine Dragons",
+    img: "a22",
+    src: "what"
+  },
+  {
+    name: "Anywhere",
+    artist: "Rita Ora",
+    img: "a23",
+    src: "rita"
+  },
+  {
+    name: "Capsize",
+    artist: "Frenship X Emily Warren",
+    img: "a24",
+    src: "capsize"
+  },
+  {
+    name: "Higher Love",
+    artist: "Kygo X Whitney Houston",
+    img: "a25",
+    src: "higher"
+  },
+  {
+    name: "I'II be there",
+    artist: "Jess Glyne",
+    img: "a26",
+    src: "jess"
+  },
+  {
+    name: "Till I Collapse",
+    artist: "Eminem X Nate Dogg",
+    img: "a27",
+    src: "collapse"
+  },
+  {
+    name: "Dear Mama",
+    artist: "2Pac",
+    img: "a28",
+    src: "pac"
+  },
+  {
+    name: "Moral of the Story",
+    artist: "Ashe",
+    img: "a29",
+    src: "Ashe"
+  },
+  {
+    name: "Gangsta's Paradise",
+    artist: "Coolio X LV",
+    img: "a30",
+    src: "coolio"
+  },
+  {
+    name: "Snap",
+    artist: "Rosa Linn",
+    img: "a31",
+    src: "Rosa"
+  },
+];
